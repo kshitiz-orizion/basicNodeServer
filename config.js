5f04f0f20b7e89466534181000f89d3d2936c866
kshitiz-orizion <kshitiz.orizion@gmail.com>
2017-02-26 14:53:00 +0530

    loopback

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
-    'facebook': {
-        clientID: '221619694973461',
-        clientSecret: '83794c44436b998ddf77e282ad1a1299',
-        callbackURL: 'https://localhost:3443/users/facebook/callback'
-    }
-}
